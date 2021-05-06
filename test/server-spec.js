de1583ff49e3e16ed44ea39f2ae783111a106428
Szymon <szymon.karpecki@gmail.com>
2021-05-06 16:47:27 +0200

    Test problem resovled

--- test/server-spec.js
+++ test/server-spec.js
@@ -8,32 +8,25 @@ var expect = chai.expect;
 describe('Server', function (){
   
   it('Should succesfully get the home page', function (){
-    chai.request(url).get('/')
+    return chai.request(url)
+    .get('/')
     .then(function (res) {
       expect(res).to.have.status(200);
-    }, function (err) {
-      throw err;
+    }) 
+    .catch(function (err) {
+      return Promise.reject(err);
     });
   });
   
   it('Should return an html file as default', function () {
-    chai.request(url).get('/')
+    return chai.request(url)
+    .get('/')
       .then(function (res) {
         expect(res).to.be.html;
-      }, function (err) {
-        throw err
+      })
+      .catch(function (err) {
+        return Promise.reject(err);
       });
   });
-
-  it('Should faiil if first test pass', function (){
-    chai.request(url).get('/')
-    .then(function (res) {
-      expect(res).to.have.status(404);
-    }, function (err) {
-      throw err;
-    });
-  });
-  
-  
-  
+    
 });
